3807ad9e19cbfeacb84e77a8435a79fde77e97e8
wiemarko <81916976+wiemarko@users.noreply.github.com>
2025-03-12 23:56:11 +0300

    twoday

--- main.js
+++ main.js
@@ -1,29 +1,56 @@
-document.addEventListener('DOMContentLoaded', async function () {
-    const usdElement = document.getElementById('usd');
-    const eurElement = document.getElementById('eur');
-    const onsElement = document.getElementById('ons');
-    const graElement = document.getElementById('gra');
-    const ceyElement = document.getElementById('cey');
-    const yarElement = document.getElementById('yar');
-    const tamElement = document.getElementById('tam');
-    const cumElement = document.getElementById('cum');
-    const gbpElement = document.getElementById('gbp');
-    const chfElement = document.getElementById('chf');
-    const rubElement = document.getElementById('rub');
-    const nokElement = document.getElementById('nok');
-    const aedElement = document.getElementById('aed');
-    const audElement = document.getElementById('aud');
-    const sekElement = document.getElementById('sek');
-    const jpyElement = document.getElementById('jpy');
-    const kwdElement = document.getElementById('kwd');
-    const bhdElement = document.getElementById('bhd');
-    const sarElement = document.getElementById('sar');
-    const dkkElement = document.getElementById('dkk');
-    const ırrElement = document.getElementById('ırr');
+// Önceki değerleri saklamak için bir nesne
+let previousValues = {
+  usd: null,
+  eur: null,
+  ons: null,
+  gra: null,
+  cey: null,
+  yar: null,
+  tam: null,
+  cum: null,
+  gbp: null,
+  chf: null,
+  rub: null,
+  nok: null,
+  aed: null,
+  aud: null,
+  sek: null,
+  jpy: null,
+  kwd: null,
+  bhd: null,
+  sar: null,
+  dkk: null,
+  ırr: null
+};
 
-    try {
+
+
+document.addEventListener('DOMContentLoaded', async function () {
+  const usdElement = document.getElementById('usd');
+  const eurElement = document.getElementById('eur');
+  const onsElement = document.getElementById('ons');
+  const graElement = document.getElementById('gra');
+  const ceyElement = document.getElementById('cey');
+  const yarElement = document.getElementById('yar');
+  const tamElement = document.getElementById('tam');
+  const cumElement = document.getElementById('cum');
+  const gbpElement = document.getElementById('gbp');
+  const chfElement = document.getElementById('chf');
+  const rubElement = document.getElementById('rub');
+  const nokElement = document.getElementById('nok');
+  const aedElement = document.getElementById('aed');
+  const audElement = document.getElementById('aud');
+  const sekElement = document.getElementById('sek');
+  const jpyElement = document.getElementById('jpy');
+  const kwdElement = document.getElementById('kwd');
+  const bhdElement = document.getElementById('bhd');
+  const sarElement = document.getElementById('sar');
+  const dkkElement = document.getElementById('dkk');
+  const ırrElement = document.getElementById('ırr');
+
+  try {
       await fetchAndUpdateData(); // İlk veri çekme
-    } catch (error) {
+  } catch (error) {
       console.error('Hata:', error);
       usdElement.textContent = 'Veri yüklenemedi';
       eurElement.textContent = 'Veri yüklenemedi';
@@ -46,15 +73,15 @@ document.addEventListener('DOMContentLoaded', async function () {
       sarElement.textContent = 'Veri yüklenemedi';
       dkkElement.textContent = 'Veri yüklenemedi';
       ırrElement.textContent = 'Veri yüklenemedi';
-    }
-  });
+  }
+});
 
-  // Veriyi 5 saniyede bir güncelleme
-  setInterval(fetchAndUpdateData, 5000);
+// Veriyi 5 saniyede bir güncelleme
+setInterval(fetchAndUpdateData, 5000);
 
-  // Veri çekme ve güncelleme işlemi
-  async function fetchAndUpdateData() {
-    try {
+// Veri çekme ve güncelleme işlemi
+async function fetchAndUpdateData() {
+  try {
       const response = await fetch('https://finans.truncgil.com/v4/today.json');
       const data = await response.json();
 
@@ -81,32 +108,30 @@ document.addEventListener('DOMContentLoaded', async function () {
       const dkkBuying = parseFloat(data.DKK.Buying).toFixed(2);
       const ırrBuying = parseFloat(data.IRR.Buying).toFixed(5);
 
+      // HTML'deki elemanları güncelle ve renk değişikliğini uygula
+      updateElement('usd', usdBuying, `Dolar: ${usdBuying} TRY`);
+      updateElement('eur', eurBuying, `Euro: ${eurBuying} TRY`);
+      updateElement('ons', onsBuying, `Altın: ${onsBuying} TRY`);
+      updateElement('gra', graBuying, `Gram Altın: ${graBuying} TRY`);
+      updateElement('cey', ceyBuying, `Çeyrek Altın: ${ceyBuying} TRY`);
+      updateElement('yar', yarBuying, `Yarım Altın: ${yarBuying} TRY`);
+      updateElement('tam', tamBuying, `Tam Altın: ${tamBuying} TRY`);
+      updateElement('cum', cumBuying, `Cumhuriyet Altını: ${cumBuying} TRY`);
+      updateElement('gbp', gbpBuying, `İngiliz Sterlini: ${gbpBuying} TRY`);
+      updateElement('chf', chfBuying, `İsviçre Frangı: ${chfBuying} TRY`);
+      updateElement('rub', rubBuying, `Rus Rublesi: ${rubBuying} TRY`);
+      updateElement('nok', nokBuying, `Norveç Kronu: ${nokBuying} TRY`);
+      updateElement('aed', aedBuying, `Birleşik Arap Emirlikleri: ${aedBuying} TRY`);
+      updateElement('aud', audBuying, `Avustralya Doları: ${audBuying} TRY`);
+      updateElement('sek', sekBuying, `İsveç Kronu: ${sekBuying} TRY`);
+      updateElement('jpy', jpyBuying, `Japon Yeni: ${jpyBuying} TRY`);
+      updateElement('kwd', kwdBuying, `Kuveyt Dinarı: ${kwdBuying} TRY`);
+      updateElement('bhd', bhdBuying, `Bahreyn Dinarı: ${bhdBuying} TRY`);
+      updateElement('sar', sarBuying, `Suudi Arabistan Riyali: ${sarBuying} TRY`);
+      updateElement('dkk', dkkBuying, `Danimarka Kronu: ${dkkBuying} TRY`);
+      updateElement('ırr', ırrBuying, `İran Riyali: ${ırrBuying} TRY`);
 
-      // HTML'deki elemanları güncelle
-      document.getElementById('usd').textContent = `Dolar: ${usdBuying} TRY`;
-      document.getElementById('eur').textContent = `Euro: ${eurBuying} TRY`;
-      document.getElementById('ons').textContent = `Altın: ${onsBuying} TRY`;
-      document.getElementById('gra').textContent = `Gram Altın: ${graBuying} TRY`;
-      document.getElementById('cey').textContent = `Çeyrek Altın: ${ceyBuying} TRY`;
-      document.getElementById('yar').textContent = `Yarım Altın: ${yarBuying} TRY`;
-      document.getElementById('tam').textContent = `Tam Altın: ${tamBuying} TRY`;
-      document.getElementById('cum').textContent = `Cumhuriyet Altını: ${cumBuying} TRY`;
-      document.getElementById('gbp').textContent = `İngiliz Sterlini: ${gbpBuying} TRY`;
-      document.getElementById('chf').textContent = `İsviçre Frangı: ${chfBuying} TRY`;
-      document.getElementById('rub').textContent = `Rus Rublesi: ${rubBuying} TRY`;
-      document.getElementById('nok').textContent = `Norveç Kronu: ${nokBuying} TRY`;
-      document.getElementById('aed').textContent = `Birleşik Arap Emirlikleri: ${aedBuying} TRY`;
-      document.getElementById('aud').textContent = `Avustralya Doları: ${audBuying} TRY`;
-      document.getElementById('sek').textContent = `İsveç Kronu: ${sekBuying} TRY`;
-      document.getElementById('jpy').textContent = `Japon Yeni: ${jpyBuying} TRY`;
-      document.getElementById('kwd').textContent = `Kuveyt Dinarı: ${kwdBuying} TRY`;
-      document.getElementById('bhd').textContent = `Bahreyn Dinarı: ${bhdBuying} TRY`;
-      document.getElementById('sar').textContent = `Suudi Arabistan Riyali: ${sarBuying} TRY`;
-      document.getElementById('dkk').textContent = `Danimarka Kronu: ${dkkBuying} TRY`;
-      document.getElementById('ırr').textContent = `İran Riyali: ${ırrBuying} TRY`;
-
-
-    } catch (error) {
+  } catch (error) {
       console.error('Veri çekme hatası:', error);
       document.getElementById('usd').textContent = 'Dolar: Veri bulunamadı TRY';
       document.getElementById('eur').textContent = 'Euro: Veri bulunamadı TRY';
@@ -126,5 +151,49 @@ document.addEventListener('DOMContentLoaded', async function () {
       document.getElementById('sar').textContent = 'Suudi Arabistan Riyali: Veri bulunamadı TRY';
       document.getElementById('dkk').textContent = 'Danimarka Kronu: Veri bulunamadı TRY';
       document.getElementById('ırr').textContent = 'İran Riyali: Veri bulunamadı TRY';
-    }
   }
+}
+
+// Elemanları güncelleme ve renk değişikliğini uygulama fonksiyonu
+function updateElement(id, newValue, text) {
+  const element = document.getElementById(id);
+  const oldValue = previousValues[id];
+
+  // Değerleri güncelle
+  element.textContent = text;
+
+  // Renk değişimi
+  if (oldValue !== null) {
+      if (newValue > oldValue) {
+          element.classList.remove('decrease');
+          element.classList.add('increase'); // Yeşil
+      } else if (newValue < oldValue) {
+          element.classList.remove('increase');
+          element.classList.add('decrease'); // Kırmızı
+      } else {
+          // Değişiklik yoksa renkleri sıfırla
+          element.classList.remove('increase', 'decrease');
+      }
+  }
+
+  // Önceki değeri güncelle
+  previousValues[id] = newValue;
+}
+
+window.onload = function () {
+  // Sayfa tamamen yüklendiğinde çalışacak kod
+  setTimeout(function () {
+      document.getElementById('loading-page').style.display = 'none';
+      document.getElementById('main-content').style.display = 'block';
+  }, 3500); // 3 saniye sonra loading sayfasını gizle
+};
+
+lottie.loadAnimation({
+  container: document.getElementById('lottie-loader'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets9.lottiefiles.com/packages/lf20_raiw2hpe.json' // Örnek Lottie JSON dosyası
+});
+
+
